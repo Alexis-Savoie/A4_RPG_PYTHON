c7d0ac7cb35791ce633162a705eccc76267de633
Allan BUSI <busi.travail@gmail.com>
2021-01-04 20:58:32 +0100

    login

--- Database/Database.py
+++ Database/Database.py
@@ -1,1 +1,54 @@
-#  connecte database
+import mysql.connector
+
+class Database:
+    # Param object
+    def __init__(self,user,password,host,database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
+        self.mydb = mysql.connector.connect(host = self.host, user = self.user,password = self.password, database = self.database)
+        
+    # Select where
+    def select(self, element, table, where):
+        if(where != ""):
+            mycursor = self.mydb.cursor()
+            sql = "SELECT "+element+" FROM "+table+" WHERE "+where
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            return myresult
+        else:
+            mycursor = self.mydb.cursor()
+            sql = "SELECT "+element+" FROM "+table
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            return myresult
+        
+    # Insert data
+    def insert(self, table, values, value):
+        mycursor = self.mydb.cursor()
+        sql = "INSERT INTO {} VALUES({})".format(table, values);
+        val = value
+        mycursor.execute(sql, val)
+        self.mydb.commit()
+        return mycursor.rowcount
+    
+    # update data
+    def update(self,where,table, values, value):
+        mycursor = self.mydb.cursor()
+        sql = "UPDATE {} SET {} WHERE {}".format(table, values, where);
+        val = value
+        mycursor.execute(sql, val)
+        self.mydb.commit()
+        return mycursor.rowcount
+    
+    # Isset variable in value null
+    def isset(self, data):
+        if data is None:
+            return ""
+        else:
+            return data
+        
+    # Off connecte
+    def disconnect(self):
+        self.mydb.close()
--- Graphique/Login.py
+++ Graphique/Login.py
@@ -1,1 +1,48 @@
-# login
+from tkinter import messagebox
+from tkinter import *
+from PIL import ImageTk
+
+class Login:
+    def __init__(self, root):
+        # Frame
+        self.root = root
+        self.root.title("WAR RPG")
+        self.root.geometry("1600x600")
+        self.bg = ImageTk.PhotoImage(file="../img/bg.jpg")
+        self.bg_image=Label(self.root, image=self.bg).place(x=0,y=0,relwidth=1,relheight=1)
+        #color
+        Blanc="#fff"
+        Gris="#fff"
+        Noir="#000"
+        Police="Impact"
+        # Frame Login
+        BlockLogin=Frame(self.root,bg=Blanc)
+        BlockLogin.place(x=100,y=300,height=500, width=700)
+        Label(BlockLogin, text="Connexion", font=(Police, 55, "bold"), fg="#bfb557", bg=Blanc).place(x=180,y=10)
+        Label(BlockLogin, text="To back young deception", font=(Police, 25, "bold"), fg="#bfb557", bg=Blanc).place(x=180,y=100)
+        Label(BlockLogin, text="Username", font=(Police, 20, "bold"), fg=Noir, bg=Blanc).place(x=90,y=170)
+        self.InputEmail=Entry(BlockLogin, font=(Police, 15),text="email", bg=Gris, bd=0)
+        self.InputEmail.insert(0, "votre@email.com")
+        self.InputEmail.place(x=90,y=230)
+        Label(BlockLogin, text="Password", font=(Police, 20, "bold"), fg=Noir, bg=Blanc).place(x=90,y=300)
+        self.InputPassword=Entry(BlockLogin, font=(Police, 15),show="*",text="password", bg=Gris, bd=0)
+        self.InputPassword.insert(0, "password")
+        self.InputPassword.place(x=90,y=350)
+        Button(BlockLogin, font=(Police, 15),text="forgot password ?", bg=Blanc, bd=0).place(x=400,y=400)
+        Button(self.root, font=(Police, 30),command=self.alert,text="go to war",cursor="pirate", bg="#bfb557", bd=0,fg=Blanc).place(x=350,y=750)
+        
+    def alert(self):
+        if self.InputEmail.get() == "" or self.InputPassword.get() == "":
+            messagebox.showerror('error', "email / password invalide")
+        elif self.InputEmail.get() == "votre@emai.com" or self.InputPassword.get() == "password":
+            messagebox.showerror('error', "email / password invalide")
+        else:
+            messagebox.showerror('success', "Super")
+
+
+            
+
+        
+root=Tk()
+obj=Login(root)
+root.mainloop()
